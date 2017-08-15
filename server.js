3220c7f161ec4539691966e9925036b24479cbf3
amagi <pau_macasaet15@yahoo.com>
2017-08-15 17:57:16 +0800

    initial commit, trying express with body-parser

--- server.js
+++ server.js
@@ -1,40 +1,25 @@
-//require the express nodejs module
-var express = require('express'),
-	//set an instance of exress
-	app = express(),
-	//require the body-parser nodejs module
-	bodyParser = require('body-parser'),
-	//require the path nodejs module
-	path = require("path");
-	
-//support parsing of application/json type post data
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = 3000;
+const hostname = 'localhost';
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/www'));
 
-//support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-//tell express that www is the root of our public web folder
-app.use(express.static(path.join(__dirname, 'www')));
-
-//tell express what to do when the /about route is requested
-app.post('/form',function(req, res){
+app.post('/form', function(req, res){
 	res.setHeader('Content-Type', 'application/json');
-
-	//mimic a slow network connection
+	
 	setTimeout(function(){
-
 		res.send(JSON.stringify({
 			firstName: req.body.firstName || null,
 			lastName: req.body.lastName || null
 		}));
-
-	}, 1000)
-
-	//debugging output for the terminal
-	console.log('you posted: First Name: ' + req.body.firstName + ', Last Name: ' + req.body.lastName);
+	}, 1000);
 });
 
-//wait for a connection
-app.listen(3000, function () {
-  console.log('Server is running. Point your browser to: http://localhost:3000');
+app.listen(port, hostname, function(){
+	console.log(`Server at http://${hostname}:${port}/`);
 });
